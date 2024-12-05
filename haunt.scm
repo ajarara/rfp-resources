2277b566be350b11564ce219a6cb2068f8e11c3f
Ahmad Jarara <git@ajarara.io>
2024-12-04 20:02:09 -0500

    Add terminal css, flesh out index

--- haunt.scm
+++ haunt.scm
@@ -5,9 +5,21 @@
 
 
 (define (my-template site title body)
-  body)
+  `((doctype html)
+    (head
+     (meta (@ (charset "utf-8")))
+     (meta (@ (name "viewport")
+              (content "width=device-width, initial-scale=1"))))
+    (title ,title)
+    (link (@ (rel "stylesheet")
+             (href "https://unpkg.com/terminal.css@0.7.4/dist/terminal.min.css")))
+    (body (@ (class "terminal"))
+          (div (@ (class "container")))
+          ,body)))
 
 (site #:title "Rockaway for Palestine"
       #:readers (list commonmark-reader)
       #:build-directory "docs"
       #:builders (list (flat-pages "in" #:template my-template)))
+
+
